dee3e36c0d846f2d8798604f2b226fd09ccb58f7
Mahendra Liya <liyamahendra4@gmail.com>
2021-05-02 23:56:57 +0530

    Updated TextInput color

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -121,13 +121,7 @@ const GameScreen = ({ navigation, route }) => {
         simulateDiceRoll();
       }
     });
-
-    return () => {
-      RNShake.removeEventListener('ShakeEvent', () => {
-        simulateDiceRoll();
-      });
-    };
-  }, [simulateDiceRoll]);
+  }, []);
 
   const restartGame = () => {
     navigation.dispatch(
